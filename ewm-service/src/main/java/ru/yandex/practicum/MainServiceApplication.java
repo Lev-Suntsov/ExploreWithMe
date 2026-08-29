fbd92f6686eb43708db4b488c499f7ffb93c96bb
Lev <leviksun.s@gmail.com>
2026-08-29 13:57:05 +0300

    испрвил repository

--- ewm-service/src/main/java/ru/yandex/practicum/MainServiceApplication.java
+++ ewm-service/src/main/java/ru/yandex/practicum/MainServiceApplication.java
@@ -2,10 +2,15 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "ru.yandex.practicum")
+@EntityScan(basePackages = "ru.yandex.practicum")            
 public class MainServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApplication.class, args);
     }
 }
+

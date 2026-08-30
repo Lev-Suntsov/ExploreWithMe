10cdcefdacbf35e2de9dc5804392bb9fbcde355c
Lev <leviksun.s@gmail.com>
2026-08-30 15:55:41 +0300

    испраил объявление репозитория

--- ewm-service/src/main/java/ru/yandex/practicum/service/impl/UserServiceImpl.java
+++ ewm-service/src/main/java/ru/yandex/practicum/service/impl/UserServiceImpl.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    UserRepository repository;
+    private final UserRepository repository;
 
     @Transactional
     @Override

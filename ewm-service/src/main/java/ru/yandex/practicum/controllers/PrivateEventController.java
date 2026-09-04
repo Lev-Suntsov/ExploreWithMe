919a1f16c12ff80a62ece41b65a5c381ece31a23
Lev <leviksun.s@gmail.com>
2026-09-05 00:15:20 +0300

    поправил контроллер

--- ewm-service/src/main/java/ru/yandex/practicum/controllers/PrivateEventController.java
+++ ewm-service/src/main/java/ru/yandex/practicum/controllers/PrivateEventController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import javax.validation.constraints.NotNull;
 
 @RestController
-@RequestMapping("/users/events")
+@RequestMapping("/users/{userId}/events")
 @RequiredArgsConstructor
 @Validated
 public class PrivateEventController {
